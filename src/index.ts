04fad7e42058cafd05e393bb0962ead4f68b57dc
ih-entpr <ilie_h@proton.me>
2024-01-02 13:55:29 +0100

    added interfaces

---
+++ src/index.ts
@@ -0,0 +1,90 @@
+export {
+  IAuthPayload,
+  IAuth,
+  IAuthDocument,
+  IAuthBuyerMessageDetails,
+  IEmailMessageDetails,
+  ISignUpPayload,
+  ISignInPayload,
+  IForgotPassword,
+  IResetPassword,
+  IReduxAuthPayload,
+  IReduxAddAuthUser,
+  IReduxLogout,
+  IAuthResponse,
+  IAuthUser,
+
+} from './interfaces/auth.interface'
+
+export {
+  IBuyerDocument,
+  IReduxBuyer,
+
+} from './interfaces/buyer.interface'
+
+export {
+  IConversationDocument,
+  IMessageDocument,
+  IMessageDetails,
+  IChatBoxProps,
+  IChatSellerProps,
+  IChatBuyerProps,
+  IChatMessageProps,
+
+} from './interfaces/chat.interface'
+
+export {
+  IEmailLocals
+} from './interfaces/email.interface'
+
+export {
+  IOffer,
+  IExtendedDelivery,
+  IDeliveredProduct,
+  IOrderEvents,
+  IOrderReview,
+  IOrderMessage,
+  IOrderDocument,
+  IOrderNotifcation,
+} from './interfaces/order.interface'
+
+export {
+  ICreateProduct,
+  ISellerProduct,
+  IProductContext,
+  IProductsProps,
+  IProductCardItems,
+  ISelectedBudget,
+  IProductViewReviewsProps,
+  IProductInfo,
+  IProductTopProps,
+} from './interfaces/product.interface'
+
+export {
+  IReviewMessageDetails,
+  IRatingTypes,
+  IReviewDocument,
+  IRatingCategoryItem,
+  IRatingCategories,
+
+
+} from './interfaces/review.interface'
+
+export {
+  ISearchResult,
+  IHitsTotal,
+  IQueryList,
+  IQueryString,
+  ITerm,
+  IPaginateProps
+} from './interfaces/search.interface'
+
+export {
+SellerType,
+  ILanguage,
+  IExperience,
+  IEducation,
+  ICertificate,
+  ISellerDocument,
+} from './interfaces/seller.interface'
+
